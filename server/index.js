31a7203930b6bf3078405d5ad28d6419b121dcc4
4c3x <136491457+4c3x@users.noreply.github.com>
2025-03-17 20:30:46 +0100

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { spawn } = require('child_process');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Resume Tweaker server is running!');
+});
+
+app.post('/tweak', (req, res) => {
+  const { resume, jobDesc } = req.body;
+  console.log('Received resume:', resume);
+  console.log('Received jobDesc:', jobDesc);
+
+  // Call Python script
+  const py = spawn('python', ['../ai/tweak.py', JSON.stringify({ resume, jobDesc })]);
+  let tweakedResume = '';
+
+  py.stdout.on('data', (data) => {
+    tweakedResume += data.toString();
+  });
+
+  py.stderr.on('data', (data) => {
+    console.error('Python error:', data.toString());
+  });
+
+  py.on('close', (code) => {
+    if (code === 0) {
+      res.json({ message: 'Tweaked', tweakedResume });
+    } else {
+      res.status(500).json({ error: 'Python script failed' });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
