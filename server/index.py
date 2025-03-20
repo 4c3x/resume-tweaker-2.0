83341cb15b2f45062d2d20a96ac146588b56ab58
ace1k <52385693+ace1k@users.noreply.github.com>
2025-03-20 19:21:01 +0100

    Switch server to Flask for Render deploy

---
+++ server/index.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/tweak', methods=['POST'])
+def tweak():
+    data = request.get_json()
+    tweaked_resume = f"Tweaked: {data['resume']}"
+    return jsonify({'message': 'Tweaked', 'tweakedResume': tweaked_resume})
