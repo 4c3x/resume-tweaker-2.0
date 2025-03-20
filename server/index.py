cb4a272d62afb59b8ed0497680cff1990e50789f
ace1k <52385693+ace1k@users.noreply.github.com>
2025-03-20 19:58:48 +0100

    Fix Flask app for Render Gunicorn

--- server/index.py
+++ server/index.py
@@ -7,3 +7,6 @@ def tweak():
     data = request.get_json()
     tweaked_resume = f"Tweaked: {data['resume']}"
     return jsonify({'message': 'Tweaked', 'tweakedResume': tweaked_resume})
+
+if __name__ == '__main__':
+    app.run()
