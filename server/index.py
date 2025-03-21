744c05e109b0f0a0a58bdf0bd8118c10e4c77cc8
ace1k <52385693+ace1k@users.noreply.github.com>
2025-03-21 18:57:34 +0100

    updated CORS header manually

--- server/index.py
+++ server/index.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
-CORS(app)  # Allow all origins for now
 
-@app.route('/tweak', methods=['POST'])
+@app.route('/tweak', methods=['POST', 'OPTIONS'])
 def tweak():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'POST'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
+
     data = request.get_json()
     tweaked_resume = f"Tweaked: {data['resume']}"
-    return jsonify({'message': 'Tweaked', 'tweakedResume': tweaked_resume})
+    response = jsonify({'message': 'Tweaked', 'tweakedResume': tweaked_resume})
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
