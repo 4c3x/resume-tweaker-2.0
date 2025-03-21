9bd645089d064592ac8a2dc19119a127bee9aa86
ace1k <52385693+ace1k@users.noreply.github.com>
2025-03-21 18:51:15 +0100

    Add CORS support to Flask srver

--- server/index.py
+++ server/index.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Allow all origins for now
 
 @app.route('/tweak', methods=['POST'])
 def tweak():
     data = request.get_json()
     tweaked_resume = f"Tweaked: {data['resume']}"
     return jsonify({'message': 'Tweaked', 'tweakedResume': tweaked_resume})
-
-if __name__ == '__main__':
-    app.run()
